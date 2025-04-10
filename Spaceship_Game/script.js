6df5c9c7ca6c961d3f2fbb9772369df93f8fab45
OmerOnn <omeronn12@gmail.com>
2025-04-10 10:12:22 +0300

    register just need date of birth

---
+++ Spaceship_Game/script.js
@@ -0,0 +1,32 @@
+// Change the screens of the menu
+function showScreen(screen_id){
+    const screens = document.querySelectorAll('.screen');
+    screens.forEach(screen => {
+        screen.style.display = 'none';
+    });
+
+    const selectedScreen = document.getElementById(screen_id);
+    if (selectedScreen) {
+        selectedScreen.style.display = 'flex';
+    }
+}
+
+let users_array = [{'p':'testuser'}]
+// Check the Register
+function registerUser(event){
+    event.preventDefault();
+
+    const password = document.getElementById('reg-password').value;
+    const confirm = document.getElementById('reg-confirm').value;
+
+    if(password !== confirm){
+      alert("Passwords dont match!")
+      return;
+    }
+
+    const user = document.getElementById('reg-username').value;
+    users_array.push({user:password})
+    alert('Register Success!');
+
+    showScreen('login');
+  }
